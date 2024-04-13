52f8f4887f55fe40a86c0ebd9cac0f93ef357f75
Bruno Eduardo <yuuidevil@gmail.com>
2024-04-12 20:16:41 -0400

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const nomes =[]
+const numeros=[]
+const form = document.getElementById('form')
+let linhas = ''
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+
+    atualizaLinha()
+})
+
+function atualizaLinha(){
+    const inputNome= document.getElementById('tnome')
+    const inputNumero=document.getElementById('telefone')
+
+    const nome = inputNome.value
+    const numero = inputNumero.value  
+    
+    
+    if(nomes.includes(inputNome.value) || numeros.includes(Number(inputNumero.value) || inputNumero.value.length<=7)){
+        alert(`o nome ${inputNome.value} ou o numero ${inputNumero.value} ja foram digitados.`)
+    } else {
+        nomes.push(inputNome.value)
+        numeros.push(Number(inputNumero.value))
+        
+
+        let linha= '<tr>'
+        linha += `<td> ${inputNome.value}</td>`
+        linha += `<td class = "left"> ${inputNumero.value}</td>`
+        linha += `</tr>`
+        linhas+= linha
+
+        const corpoTabela = document.querySelector('tbody')
+        corpoTabela.innerHTML =linhas
+    }
+    inputNome.value=''
+    inputNumero.value= ''
+    inputNome.focus()
+}
